4ccadbb10832bb924f35b03fb3d35cc4b065eadf
Diego Ribeiro <dnr.new@gmail.com>
2020-02-03 22:47:20 -0300

    Section 11 | Criação de exceptions personalizadas
    Cria exceptions personalizadas através do DomainException e utilizando o
    AplicationException. Além disso, utilizao bloco try e catch para a
    tentativa e captura da exceptions, bem como a classe Exception que são
    exceções de sistema.

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,66 +1,54 @@
 ﻿using Course.Entities;
+using Course.Entities.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 
 namespace Course
 {
-    class Program
+    class ProcessFile
     {
         public static void Main(string[] args)
         {
-            List<TaxPayer> listTaxPayer = new List<TaxPayer>();
-
-            Console.Write("Enter the number of tax payers: ");
-            int quantity = int.Parse(Console.ReadLine());
-
-            for (int i = 1; i <= quantity; i++)
+            try
             {
-                Console.WriteLine($"Tax payer #{i} data: ");
-                Console.Write("Individual or company (i/c)? ");
-                char typePayer = char.Parse(Console.ReadLine());
+                Account account = new Account();
 
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
+                Console.WriteLine("Enter account data: ");
+                Console.Write("Number: ");
+                account.Number = int.Parse(Console.ReadLine());
 
-                Console.Write("Anual income: ");
-                double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Holder: ");
+                account.Holder = Console.ReadLine();
 
-                if (typePayer == 'i')
-                {
-                    Console.Write("Health expenditures: ");
-                    double healthExpenditures = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Initial balance: ");
+                account.Balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-                    listTaxPayer.Add(new Individual(name, anualIncome, healthExpenditures));
-                }
-                else if (typePayer == 'c')
-                {
-                    Console.Write("Number of employees: ");
-                    int numberEmployees = int.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                account.WithdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-                    listTaxPayer.Add(new Company(name, anualIncome, numberEmployees));
-                }
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.Withdraw(amount);
+
+                Console.WriteLine();
+                Console.WriteLine("New balance:" + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+
+                Console.ReadKey();
             }
-
-            Console.WriteLine();
-            Console.WriteLine("TAXES PAID: ");
-
-            var sumTaxPayer = 0.0;
-
-            foreach (TaxPayer taxPayer in listTaxPayer)
+            catch (DomainException ex)
             {
-                taxPayer.AnualIncome = taxPayer.Tax();
-                sumTaxPayer += taxPayer.AnualIncome;
-
-                Console.WriteLine(
-                    taxPayer.Name + ": $ " 
-                    + taxPayer.AnualIncome.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Withdraw error: " + ex.Message);
             }
-
-            Console.WriteLine();
-            Console.WriteLine("TOTAL TAXES: $ " + sumTaxPayer.ToString("F2", CultureInfo.InvariantCulture));
-
-            Console.ReadKey();
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Format error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unexpected error: " + ex.Message);
+            }
         }
     }
 }
