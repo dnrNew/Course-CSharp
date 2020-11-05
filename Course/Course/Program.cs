5d08b4daf275c38c5cc5d7f99d08fc5d886059c4
Diego Nogueira <dnr.new@gmail.com>
2020-11-04 22:08:40 -0300

    Inicializa testes de comportamento com as classes de arquivos
    Testando as opções das classes File, FileInfo, FileStream, StreamReader
    e IOException.

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,10 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace Course
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            void Teste1()
+            {
+                string sourcePath = @"C:\Temp\file1.txt";
+                string targetPath = @"C:\Temp\file2.txt";
+
+                try
+                {
+                    FileInfo fileInfo = new FileInfo(sourcePath);
+                    fileInfo.CopyTo(targetPath);
+
+                    string[] lines = File.ReadAllLines(sourcePath);
+
+                    foreach (string line in lines)
+                    {
+                        Console.WriteLine();
+                    }
+
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("An error occurred");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            void Teste2()
+            {
+                string path = @"C:\Temp\file1.txt";
+                FileStream fs = null;
+                StreamReader sr = null;
+
+                try
+                {
+                    fs = new FileStream(path, FileMode.Open);
+                    sr = new StreamReader(fs);
+
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("An error occurred");
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    if (sr != null) sr.Close();
+                    if (fs != null) sr.Close();
+                }
+            }
+
+            Teste3();
+
+            void Teste3()
+            {
+                string path = @"C:\Temp\file1.txt";
+                StreamReader sr = null;
+
+                try
+                {
+                    sr = File.OpenText(path);
+
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("An error occurred");
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    if (sr != null) sr.Close();
+                }
+            }
+        }
     }
 }
