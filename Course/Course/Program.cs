7bfdb489289c57c53d5e1da3c44b5fe261faa0a2
Diego Nogueira <dnr.new@gmail.com>
2020-11-09 20:59:36 -0300

    Cria teste para Bloco using, StreamWrite, Directory e Path
    Cria testes para entender o funcionamento das classes que envolvem
    arquivos utilizando o bloco using, a classe StreamWrite, Directory,
    Directoryinfo e a classe Path.

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.IO;
 
 namespace Course
@@ -60,8 +58,6 @@ namespace Course
                 }
             }
 
-            Teste3();
-
             void Teste3()
             {
                 string path = @"C:\Temp\file1.txt";
@@ -86,6 +82,99 @@ namespace Course
                 {
                     if (sr != null) sr.Close();
                 }
+            }
+
+            void Teste4()
+            {
+                string path = @"C:\Temp\file1.txt";
+
+                try
+                {
+                    using (StreamReader sr = File.OpenText(path))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("An error occurred");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            void Teste5()
+            {
+                string sourcePath = @"C:\Temp\file1.txt";
+                string targetPath = @"C:\Temp\file2.txt";
+
+                try
+                {
+                    string[] lines = File.ReadAllLines(sourcePath);
+
+                    using (StreamWriter sw = File.AppendText(targetPath))
+                    {
+                        foreach (var line in lines)
+                        {
+                            sw.WriteLine(line.ToUpper());
+                        }
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("An error occurred");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            void Teste6()
+            {
+                string path = @"C:\Temp";
+
+                try
+                {
+                    var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                    Console.WriteLine("Folders: ");
+                    foreach (string s in folders)
+                    {
+                        Console.WriteLine(s);
+                    }
+
+                    var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                    Console.WriteLine("Files: ");
+                    foreach (string s in files)
+                    {
+                        Console.WriteLine(s);
+                    }
+
+                    Directory.CreateDirectory(path + "\\NewFolder");
+                    Directory.CreateDirectory(path + @"\NewFolder2");
+                    Directory.CreateDirectory(@"C:\Temp\NewFolder3");
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("An error occurred");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Teste7();
+
+            void Teste7()
+            {
+                string path = @"C:\Temp\File5.txt";
+
+                Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+                Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+                Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+                Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+                Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+                Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+                Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+                Console.WriteLine("GetTempPath: " + Path.GetTempPath());
             }
         }
     }
