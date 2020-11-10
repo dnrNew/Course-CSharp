89ac45129274695c032989e0867bd9b8d3cc8dbf
Diego Nogueira <dnr.new@gmail.com>
2020-11-09 22:42:21 -0300

    Cria pasta com arquivo csv alterando valores
    Cria uma pasta com um arquivo chamado summary.csv para alterar seus
    valores e salvar na pasta out.
    
    Cria Classe Product para organizar os dados do arquivo na parte da
    leitura dos dados e cálculo do total das vendas para o resumo de vendas.

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Course.Entities;
+using System;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 
 namespace Course
 {
@@ -7,175 +10,39 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            void Teste1()
+            try
             {
-                string sourcePath = @"C:\Temp\file1.txt";
-                string targetPath = @"C:\Temp\file2.txt";
+                string sourceFilePath = @"C:\Temp\Vendas.csv";               
+                string[] lines = File.ReadAllLines(sourceFilePath);
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
 
-                try
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
                 {
-                    FileInfo fileInfo = new FileInfo(sourcePath);
-                    fileInfo.CopyTo(targetPath);
-
-                    string[] lines = File.ReadAllLines(sourcePath);
-
                     foreach (string line in lines)
                     {
-                        Console.WriteLine();
-                    }
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
 
-                }
-                catch (IOException ex)
-                {
-                    Console.WriteLine("An error occurred");
-                    Console.WriteLine(ex.Message);
-                }
-            }
+                        Product product = new Product(name, price, quantity);
 
-            void Teste2()
-            {
-                string path = @"C:\Temp\file1.txt";
-                FileStream fs = null;
-                StreamReader sr = null;
-
-                try
-                {
-                    fs = new FileStream(path, FileMode.Open);
-                    sr = new StreamReader(fs);
-
-                    string line = sr.ReadLine();
-                    Console.WriteLine(line);
-                }
-                catch (IOException ex)
-                {
-                    Console.WriteLine("An error occurred");
-                    Console.WriteLine(ex.Message);
-                }
-                finally
-                {
-                    if (sr != null) sr.Close();
-                    if (fs != null) sr.Close();
-                }
-            }
-
-            void Teste3()
-            {
-                string path = @"C:\Temp\file1.txt";
-                StreamReader sr = null;
-
-                try
-                {
-                    sr = File.OpenText(path);
-
-                    while (!sr.EndOfStream)
-                    {
-                        string line = sr.ReadLine();
-                        Console.WriteLine(line);
+                        sw.WriteLine(product.Name + "," + product.Total().ToString("F2", CultureInfo.InvariantCulture));
                     }
                 }
-                catch (IOException ex)
-                {
-                    Console.WriteLine("An error occurred");
-                    Console.WriteLine(ex.Message);
-                }
-                finally
-                {
-                    if (sr != null) sr.Close();
-                }
             }
-
-            void Teste4()
+            catch (IOException ex)
             {
-                string path = @"C:\Temp\file1.txt";
-
-                try
-                {
-                    using (StreamReader sr = File.OpenText(path))
-                    {
-                        while (!sr.EndOfStream)
-                        {
-                            string line = sr.ReadLine();
-                            Console.WriteLine(line);
-                        }
-                    }
-                }
-                catch (IOException ex)
-                {
-                    Console.WriteLine("An error occurred");
-                    Console.WriteLine(ex.Message);
-                }
+                Console.WriteLine("An error occurred: ");
+                Console.WriteLine(ex.Message);
             }
 
-            void Teste5()
-            {
-                string sourcePath = @"C:\Temp\file1.txt";
-                string targetPath = @"C:\Temp\file2.txt";
 
-                try
-                {
-                    string[] lines = File.ReadAllLines(sourcePath);
 
-                    using (StreamWriter sw = File.AppendText(targetPath))
-                    {
-                        foreach (var line in lines)
-                        {
-                            sw.WriteLine(line.ToUpper());
-                        }
-                    }
-                }
-                catch (IOException ex)
-                {
-                    Console.WriteLine("An error occurred");
-                    Console.WriteLine(ex.Message);
-                }
-            }
-
-            void Teste6()
-            {
-                string path = @"C:\Temp";
-
-                try
-                {
-                    var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
-                    Console.WriteLine("Folders: ");
-                    foreach (string s in folders)
-                    {
-                        Console.WriteLine(s);
-                    }
-
-                    var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
-                    Console.WriteLine("Files: ");
-                    foreach (string s in files)
-                    {
-                        Console.WriteLine(s);
-                    }
-
-                    Directory.CreateDirectory(path + "\\NewFolder");
-                    Directory.CreateDirectory(path + @"\NewFolder2");
-                    Directory.CreateDirectory(@"C:\Temp\NewFolder3");
-                }
-                catch (IOException ex)
-                {
-                    Console.WriteLine("An error occurred");
-                    Console.WriteLine(ex.Message);
-                }
-            }
-
-            Teste7();
-
-            void Teste7()
-            {
-                string path = @"C:\Temp\File5.txt";
-
-                Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
-                Console.WriteLine("PathSeparator: " + Path.PathSeparator);
-                Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
-                Console.WriteLine("GetFileName: " + Path.GetFileName(path));
-                Console.WriteLine("GetExtension: " + Path.GetExtension(path));
-                Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
-                Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
-                Console.WriteLine("GetTempPath: " + Path.GetTempPath());
-            }
         }
     }
 }
