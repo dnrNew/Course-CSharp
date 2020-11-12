e7889384a687ae7e617942a15db072a0a71cadc4
Diego Nogueira <dnr.new@gmail.com>
2020-11-11 22:04:04 -0300

    Cria classes de domínio
    Cria classes de domínio para o exercício que visa calcular contratos com
    taxas aplicando juros mensais e utilizando services com interfaces.

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -11,28 +11,7 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter rental data");
-            Console.WriteLine("Car model:");
-            string model = Console.ReadLine();
-
-            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm):");
-            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Return (dd/MM/yyyy hh:mm):");
-            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Enter price per hour: ");
-            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Enter price per day: ");
-            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
-            RentalServices rentalServices = new RentalServices(hour, day, new BrazilTaxService());
-            rentalServices.ProcessInvoice(carRental);
-
-            Console.WriteLine("\nINVOICE:");
-            Console.WriteLine(carRental.Invoice);
+            
         }
     }
 }
