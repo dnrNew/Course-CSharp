6653171df39c6a4acaff254b3e594d34b611ff9c
Diego Nogueira <dnr.new@gmail.com>
2020-11-11 21:25:55 -0300

    Solução para aluguel de carro sem interface
    Cria solução para aluguel de carro sem inteface.
    
    Cria classes CarRental, Vehicle e Invoice para definir os atributos para
    aluguel de carro
    
    Cria Services para o cálculo da taxa e do aluguel dos carros.

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using Course.Entities;
+using Course.Services;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace Course
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.WriteLine("Car model:");
+            string model = Console.ReadLine();
+
+            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm):");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Return (dd/MM/yyyy hh:mm):");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+            RentalServices rentalServices = new RentalServices(hour, day);
+            rentalServices.ProcessInvoice(carRental);
+
+            Console.WriteLine("\nINVOICE:");
+            Console.WriteLine(carRental.Invoice);
+        }
     }
 }
