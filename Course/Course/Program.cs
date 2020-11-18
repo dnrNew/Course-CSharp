34dfa89fa4210ed96c0de7243f18036299c68e81
Diego Nogueira <dnr.new@gmail.com>
2020-11-17 21:40:19 -0300

    Exemplo IComparable
    Exemplo de utilização da interface IComparable para comparar nome de
    Funcionários em um arquivo.

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,9 +1,7 @@
 ﻿using Course.Entities;
-using Course.Services;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
+using System.IO;
 
 namespace Course
 {
@@ -11,28 +9,33 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter rental data");
-            Console.WriteLine("Car model:");
-            string model = Console.ReadLine();
 
-            Console.WriteLine("Pickup (dd/MM/yyyy hh:mm):");
-            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            string path = @"C:\Temp\in.csv";
 
-            Console.WriteLine("Return (dd/MM/yyyy hh:mm):");
-            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
 
-            Console.WriteLine("Enter price per hour: ");
-            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
 
-            Console.WriteLine("Enter price per day: ");
-            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Sort();
 
-            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
-            RentalServices rentalServices = new RentalServices(hour, day, new BrazilTaxService());
-            rentalServices.ProcessInvoice(carRental);
-
-            Console.WriteLine("\nINVOICE:");
-            Console.WriteLine(carRental.Invoice);
+                    foreach (var employee in list)
+                    {
+                        Console.WriteLine(employee);
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
